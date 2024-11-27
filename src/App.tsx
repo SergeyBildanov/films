c7c8828aae48b3c5f2270ae96d2ae8b0d049b537
SergeyBildanov <troktrokov@gmail.com>
2024-11-27 13:03:38 +0400

    logic changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,29 @@ type Args = {
   count?: number,
 }
 
-function Star() {
+function Star(){
   return (
-    <li>
+    <>
       <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
         <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
         <path d="M0 0h18v18H0z" fill="none" />
       </svg>
-    </li>
+    </>
   )
 }
 function Stars({count = 0}: Args) {
-  if(typeof count !== "number" || count > 5){
+  if(typeof count !== "number" || count > 5 || count < 5){
     return null;
   }
-  let list = [1, 2, 3, 4, 5];
-  list = list.map(i => {
-    return (i > count) ? 0 : i;
-  })
-  console.log(list);
+  let list: Array<JSX.Element> = []
+  for(let i=0; i<count; i++){
+    let element = <li><Star key={i}/></li>
+    list.push(element)
+  }
   return (
     <ul className="card-body-stars u-clearfix">
-      {list.map(i => {
-        return i === 0 ? false : <Star key={i} />
+      {list.map(item => {
+        return item
       })}
     </ul>
   )
@@ -34,9 +34,7 @@ function Stars({count = 0}: Args) {
 
 function App() {
   return (
-    <>
     <Stars count={5} />
-    </>
   )
 }
 
